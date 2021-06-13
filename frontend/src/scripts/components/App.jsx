44ef25fd611c6084b6ad9ce24d9523fe1b77c627
YoucefChihi <youcef.chihi@gmail.com>
2021-06-13 15:46:19 +0200

    clarify proptypes

--- frontend/src/scripts/components/App.jsx
+++ frontend/src/scripts/components/App.jsx
@@ -8,13 +8,29 @@ import { translate } from 'react-i18next';
 class App extends Component {
   static propTypes = {
     t: PropTypes.function,
-    data: PropTypes.any,
+    data: PropTypes.shape({
+      body: PropTypes.string,
+      date: PropTypes.string,
+      from: PropTypes.shape({
+        email: PropTypes.string,
+        name: PropTypes.string,
+      }),
+      id: PropTypes.string,
+      protocolStatus: PropTypes.shape({
+        dkim: PropTypes.string,
+        spf: PropTypes.string,
+        dmarc: PropTypes.string,
+      }),
+      subject: PropTypes.string,
+      threadId: PropTypes.string,
+      wordCount: PropTypes.number
+    }),
   }
 
   render() {
     const { t, data } = this.props;
     const { counts, messages } = data;
-
+    console.log('messssss', messages)
     return (
       <div>
         <h1>{t('app:title-home')}</h1>
